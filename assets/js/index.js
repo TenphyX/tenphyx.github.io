87614fe94d6aa5abdc3bf10a0aad62d4e4ae7b9c
centos <centos@CentOS7.localdomain>
2015-03-14 08:02:45 -0400

    Blog update at 2015-03-14 12:02:45

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,103 +1,337 @@
-/**
- * Main JS file for Casper behaviours
- */
+// _gaq.push(['_trackPageview', '/some-page']);
+var currentPage = 1;
+var dateMap = [];
 
-/* globals jQuery, document */
-(function ($, sr, undefined) {
+function trackUrl(url) {
+    if (_gaq) {
+        _gaq.push(['_trackPageview', url]);
+    }
+}
+
+function cleanUrl(url) {
+    if (url.indexOf('?') !== -1) {
+        return url;
+    }
+    if (url.lastIndexOf('/') !== (url.length - 1)) {
+        url += '/';
+    }
+    return url;
+}
+
+function compare(a,b) {
+  if (a.date < b.date) { return -1; }
+  if (a.date > b.date) { return 1; }
+  return 0;
+}
+ 
+function clockTick() {
+    var currentTime = new Date ();
+    var currentHours = currentTime.getHours();
+    var currentMinutes = currentTime.getMinutes();
+    var timeOfDay = (currentHours < 12) ? "AM" : "PM";
+    currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+    currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+    currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+    var currentTimeString = currentHours + ":" + currentMinutes + " " + timeOfDay;
+    $(".dn-time").html(currentTimeString);
+    checkTimeSwitch();
+}
+
+function isValidDate(d) {
+    if ( Object.prototype.toString.call(d) !== "[object Date]" ) {
+        return false;
+    }
+    return !isNaN(d.getTime());
+}
+
+function addToMap(id) {
+    var idx = $(id).attr('data-index');
+    dateMap.unshift({id:id,idx:idx});
+}
+ 
+function addToEven(id) {
+    $(id).appendTo(".dn-even-parent");
+    $(id).removeClass('hidden');
+}
+ 
+ 
+function addToOdd(id) {
+    $(id).appendTo(".dn-odd-parent");
+    $(id).removeClass('hidden');
+}
+
+function displayPosts() {
+    dateMap = [];
+    // the formating of date is incorrect from the core is what it seems, so need to use defined
+    // index to correctly sort the posts for single column screens. 
+    var idx = 0;
+    $('.dn-post-entry').each(function() {
+        $(this).attr('data-index', idx);
+        addToMap('#'+$(this).attr('id'));
+        idx++;
+    });
+    $('.dn-even').each(function() {
+        addToEven('#'+$(this).attr('id'));
+    });
+    $('.dn-odd').each(function() {
+        addToOdd('#'+$(this).attr('id'));
+    });
+    adjustElementsToWidth();
+}
+
+function adjustElements() {
+    displayPosts();
+    
+    var blogName = $('.dn-blog-name > h5').text();
+    if (blogName.length > 40) {
+        $('.dn-blog-name > h5').text(blogName.substring(0, 40) + '...');
+    }
+    $('.dn-blog-name').removeClass('hidden');
+}
+
+function adjustElementsForBlog() {
+    $('.dn-post-excerpt').each(function() {
+        var lastPar = $(this).children(':last-child');
+        if (lastPar) {
+            lastPar.html(lastPar.html() + '...');
+        }
+    });
+}
+
+function adjustForSmall() {
+    if ($('.dn-single-column').children('article').length === 0) {
+        for(var i = 0; i < dateMap.length; i++) {
+            $(dateMap[i].id).prependTo('.dn-single-column');
+        }       
+    }
+}
+
+function adjustForMediumAndLarge() {
+    if ($('.dn-single-column').children('article').length > 0) {
+        for(var i = 0; i < dateMap.length; i++) {
+            var appendEl = '.dn-even-parent';
+            if ($(dateMap[i].id).hasClass('dn-odd')) {
+                appendEl = '.dn-odd-parent';
+            }
+            $(dateMap[i].id).prependTo(appendEl);
+        } 
+    }
+}
+
+function adjustElementsToWidth() {
+    var ww = $(window).width();
+    var isSmall = (ww <= 640);
+    var isMedium = (ww > 640 && ww <= 900);
+    $('.adjust').each(function() {
+        if (isSmall) {
+            $(this).addClass('sm-aj');
+            $(this).removeClass('md-aj');
+            adjustForSmall();
+        } else if (isMedium) {
+            $(this).addClass('md-aj');
+            $(this).removeClass('sm-aj');
+            adjustForMediumAndLarge();
+        } else {
+            $(this).removeClass('md-aj');
+            $(this).removeClass('sm-aj');
+            adjustForMediumAndLarge();
+        }
+    });
+}
+ 
+function switchTime() {
+    $('body').toggleClass('night');
+    $('body').addClass('dn-noswitch');
+}
+
+function scrollToTop(p) {
+    var d = (p || "normal");
+    $("html, body").animate({ scrollTop: 0 }, d);
+}
+
+function initActions() {
+    $('.dn-back-to-posts').click(function(event) {
+        lnkPushState($(this).attr("href"));
+        displayPageInContentSmooth('/page/' + currentPage);
+        return false;
+    });
+    $('.dn-load-post-page, .dn-post-link').click(function(event) {
+        lnkPushState($(this).attr("href"));
+        if ($(this).attr('href').indexOf('/page/') !== -1) {
+            var page = $(this).attr('href').match(/\d+/gi);
+            if (page) {
+                currentPage = parseInt(page[0]);
+            } else {
+                 currentPage = 1;
+            }
+        } else if ($(this).hasClass('dn-load-post-page')) {
+            currentPage = 1;
+        }
+        displayPageInContentSmooth($(this).attr('href'));
+        return false;
+    });
+    $('.dn-ajx-link').unbind('click').click(function(event) {
+        currentPage = 1;
+        lnkPushState($(this).attr("href"));
+        displayPageInContentSmooth($(this).attr('href'));
+        return false;
+    });
+    $('.uimg, uimg-d').unbind('click').click(function() {
+        window.location.href = '/';
+    });
+}
+
+function prepareForLoad() {
+    var load = null;
+    if ($('body').hasClass('night')) {
+        load = $('.night-load').clone();
+    } else {
+        load = $('.day-load').clone();
+    }
+    load.appendTo("#dn-content");
+    load.removeClass('hidden');
+    $("#dn-content").show();
+}
+
+function loadPageElements(data) {
+    $("#dn-content").empty(); 
+    $("#dn-content").hide();
+    var $response = $(data);
+    var content = $response.find('#dn-content').html();
+    var navMenu = $response.find('.dn-nav-menu').html();
+    $("#dn-content").append(content);
+    $('.dn-nav-menu').empty().append(navMenu);
+    initActions();
+    displayPosts();
+    adjustElementsForBlog();
+    $("#dn-content").fadeIn(function() {
+        $('.site-footer').fadeIn();
+    });
+}
+
+function displayPageInContentSmooth(url, callback) {
+    $('.site-footer').hide();
+    scrollToTop();
+    $("#dn-content").fadeOut(400, function() {
+       $("#dn-content").empty(); 
+       prepareForLoad();
+       loadPageToContent(url, callback);
+    }); 
+}
+
+function checkTimeSwitch() {
+    var dh = new Date().getHours();
+    var isNight = $('body').hasClass('night');
+    var noSwitch = $('body').hasClass('dn-noswitch');
+    if (dh >= 18 || dh <= 6) {
+        if (!isNight && !noSwitch) {
+            $('body').toggleClass('night');
+        }
+    } else {
+        if (isNight && !noSwitch) {
+            $('body').removeClass('night');
+        }
+    }
+}
+
+function displayHome() {
+    setTimeout(function() {
+        $('.dn-initial').hide();
+        $('.dn-initial').removeClass('hidden');
+        checkTimeSwitch();
+        $('.dn-initial').show();
+        adjustElementsForBlog();
+        $('body').css('opacity', '1');
+    }, 100);
+}
+
+function loadPageToContent(url, callback) {
+    trackUrl(url);
+    url = cleanUrl(url);
+    $.ajax({
+        type: "GET",
+        url: url + '?_pc_remove_args=true',
+        cache: false,
+        dataType: 'html',
+        success: function(data) {
+            setTimeout(function() {
+                loadPageElements(data);
+            }, 400);
+        },
+        error: function(){      
+            
+        },
+        complete: function(){ 
+            
+        },
+        statusCode: {
+            404: function(xhr) {
+                $("#dn-content").empty().append('<h1>404 Page not found</h1><br/><p>The page you are trying to access was not found. If you are an admin of this blog, please make sure this page is available or remove the link pointing to it.</p>');
+            }
+        }
+    });
+}
+ 
+jQuery.fn.rotate = function(degrees) {
+    $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
+        '-moz-transform' : 'rotate('+ degrees +'deg)',
+        '-ms-transform' : 'rotate('+ degrees +'deg)',
+        'transform' : 'rotate('+ degrees +'deg)',
+    });
+};
+
+function lnkPushState(url) {
+    var pushurl = url || '/';
+    history.pushState({path: pushurl}, '', pushurl);
+}
+
+$(window).bind('popstate', function(event) {
+    var state = event.originalEvent.state;
+    if (state && state.path) {
+        displayPageInContentSmooth(state.path);
+    }
+});
+
+/*globals jQuery, document */
+(function ($) {
     "use strict";
 
-    var $document = $(document),
-
-        // debouncing function from John Hann
-        // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
-        debounce = function (func, threshold, execAsap) {
-            var timeout;
-
-            return function debounced () {
-                var obj = this, args = arguments;
-                function delayed () {
-                    if (!execAsap) {
-                        func.apply(obj, args);
-                    }
-                    timeout = null;
-                }
-
-                if (timeout) {
-                    clearTimeout(timeout);
-                } else if (execAsap) {
-                    func.apply(obj, args);
-                }
-
-                timeout = setTimeout(delayed, threshold || 100);
-            };
-        };
-
-    $document.ready(function () {
-
-        var $postContent = $(".post-content");
-        $postContent.fitVids();
-
-        function updateImageWidth() {
-            var $this = $(this),
-                contentWidth = $postContent.outerWidth(), // Width of the content
-                imageWidth = this.naturalWidth; // Original image resolution
-
-            if (imageWidth >= contentWidth) {
-                $this.addClass('full-img');
+    $(document).ready(function(){
+        
+        setInterval('clockTick()', 1000);
+        initActions();
+        
+        $('.night-day-action').click(function(event) {
+            event.preventDefault();
+            switchTime();
+        });
+        
+        $('.dn-show-menu-action').click(function(event) {
+            //event.preventDefault();
+            if ($(this).hasClass('open')) {
+                $(this).removeClass('open');
+                $('.side-nav').slideDown(500, function() {
+                    $('.dn-show-menu-action > span').rotate(180);
+                });
             } else {
-                $this.removeClass('full-img');
+                $(this).addClass('open');
+                $('.side-nav').slideUp(500, function() {
+                    $('.dn-show-menu-action > span').rotate(0);
+                });
             }
-        }
-
-        var $img = $("img").on('load', updateImageWidth);
-        function casperFullImg() {
-            $img.each(updateImageWidth);
-        }
-
-        casperFullImg();
-        $(window).smartresize(casperFullImg);
-
-        $(".scroll-down").arctic_scroll();
-
-        $(".menu-button, .nav-cover, .nav-close").on("click", function(e){
-            e.preventDefault();
-            $("body").toggleClass("nav-opened nav-closed");
         });
 
+    
+
+        $(".post-content").fitVids();
+        adjustElements();
+        displayHome();
+        
+        $(window).load(function() {
+            adjustElementsToWidth();
+        }).resize(function(){
+            adjustElementsToWidth();
+        });
     });
 
-    // smartresize
-    jQuery.fn[sr] = function(fn) { return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
-
-    // Arctic Scroll by Paul Adam Davis
-    // https://github.com/PaulAdamDavis/Arctic-Scroll
-    $.fn.arctic_scroll = function (options) {
-
-        var defaults = {
-            elem: $(this),
-            speed: 500
-        },
-
-        allOptions = $.extend(defaults, options);
-
-        allOptions.elem.click(function (event) {
-            event.preventDefault();
-            var $this = $(this),
-                $htmlBody = $('html, body'),
-                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
-                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
-                toMove;
-
-            if (offset) {
-                toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
-            } else if (position) {
-                toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
-            } else {
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
-            }
-        });
-
-    };
-})(jQuery, 'smartresize');
+}(jQuery));
